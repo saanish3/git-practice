7d24d51d6b131faa921a3d8511e8a9c17c2cd244
San <san.robinxin@gmail.com>
2022-03-25 15:54:41 -0500

    adding d now

--- function_d.py
+++ function_d.py
@@ -2,7 +2,9 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    result = max(numbers)
+
+    return result 
 
 
 if __name__ == "__main__":
